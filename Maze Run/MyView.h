502eaf34a259c8e0aef7e168606880a0b16ab174
Leo is awesome <lc599@drexel.edu>
2014-08-11 00:43:50 -0400

    init commit

--- Maze Run/MyView.h
+++ Maze Run/MyView.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  MyView.h
 //  Maze Run
 //
 //  Created by Leonard Chan on 8/10/14.
@@ -7,8 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "MyView.h"
 
-@interface ViewController : UIViewController
+@interface MyView : UIView
 
 @end
--- Maze Run/ViewController.m
+++ Maze Run/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <mach/mach.h>
 
 @interface ViewController ()
 
@@ -17,7 +18,36 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"w:%f h:%f",[UIScreen mainScreen].bounds.size.width,
+          [UIScreen mainScreen].bounds.size.height);
+    
+    //MyView *mv = [[MyView alloc] initWithTestVal:testVal withFrame:self.view.frame];
+    MyView *mv = [[MyView alloc] initWithFrame:self.view.frame];
+    
+    [self setView:mv];
+    
+    [NSTimer scheduledTimerWithTimeInterval:0.01 target:self selector:@selector(drawLoop) userInfo:nil repeats:true];
+}
+
+- (void)drawLoop{
+    [self report_memory];
+    [self.view setNeedsDisplay];
+}
+
+- (void)report_memory{
+    struct task_basic_info info;
+    mach_msg_type_number_t size = sizeof(info);
+    kern_return_t kerr = task_info(mach_task_self(),
+                                   TASK_BASIC_INFO,
+                                   (task_info_t)&info,
+                                   &size);
+    if( kerr == KERN_SUCCESS ) {
+        NSNumber *num = [NSNumber numberWithUnsignedInt:info.resident_size];
+        NSLog(@"Memory in use (in megabytes): %f", num.doubleValue/1000000);
+    } else {
+        NSLog(@"Error with task_info(): %s", mach_error_string(kerr));
+    }
 }
 
 - (void)didReceiveMemoryWarning
