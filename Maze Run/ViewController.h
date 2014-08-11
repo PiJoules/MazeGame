8244ee26919204d2e2b30c54a0c0c2ab999f20e8
Leo is awesome <lc599@drexel.edu>
2014-08-10 20:43:16 -0400

    Initial Commit

---
+++ Maze Run/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Maze Run
+//
+//  Created by Leonard Chan on 8/10/14.
+//  Copyright (c) 2014 Leonard Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Maze RunTests/Maze_RunTests.m
@@ -0,0 +1,34 @@
+//
+//  Maze_RunTests.m
+//  Maze RunTests
+//
+//  Created by Leonard Chan on 8/10/14.
+//  Copyright (c) 2014 Leonard Chan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Maze_RunTests : XCTestCase
+
+@end
+
+@implementation Maze_RunTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
