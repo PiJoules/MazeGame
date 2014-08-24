1468cf9f992b888e69c75004d346cb28fd7c4b71
Leo is awesome <lc599@drexel.edu>
2014-08-23 23:46:58 -0400

    almost got first person view to work

---
+++ Maze Run/TestView.h
@@ -0,0 +1,13 @@
+//
+//  TestView.h
+//  Maze Run
+//
+//  Created by Leonard Chan on 8/23/14.
+//  Copyright (c) 2014 Leonard Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestView : UIView
+
+@end
---
+++ Maze Run/TestView.m
@@ -0,0 +1,31 @@
+//
+//  TestView.m
+//  Maze Run
+//
+//  Created by Leonard Chan on 8/23/14.
+//  Copyright (c) 2014 Leonard Chan. All rights reserved.
+//
+
+#import "TestView.h"
+
+@implementation TestView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
